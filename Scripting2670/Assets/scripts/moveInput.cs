bc75294777b6a1e4b10a1e79b80fcca4f387a3cf
AnnaEdmondson <puckpranks.cluefinder@gmail.com>
2017-08-28 14:05:59 -0600

    added scripts and movement inputs

---
+++ Scripting2670/Assets/scripts/moveInput.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class moveInput : MonoBehaviour {
+
+	// Use this for initialization
+	public static Action<float> KeyAction;
+		
+	
+	
+	// Update is called once per frame
+	void Update () {
+		if(KeyAction !=null) 
+		{
+			KeyAction(Input.GetAxis("Horizontal"));
+		}
+
+	}
+}
