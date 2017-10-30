917380978b8470faa2e832fa7911e24759231f03
AnnaEdmondson <puckpranks.cluefinder@gmail.com>
2017-10-30 14:11:42 -0600

    models in unity

---
+++ Singleton/Assets/Scripts/NewScripts/PlayerPrefs/PurchaseData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PurchaseData : GameData {
+
+	public float health;
+
+	PurchaseData(){
+		dataName = "PurchaseData";
+	}
+	
+
+}
