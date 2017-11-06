ac2714c8ca61e9df419b0ae095c172b49a7e358e
AnnaEdmondson <puckpranks.cluefinder@gmail.com>
2017-11-06 15:06:13 -0700

    pooling weapons

---
+++ Scripting2670/Assets/scripts/PoolingWeapon.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PoolingWeapon : MonoBehaviour {
+
+	public List<Transform> bullets;
+	public static 
+
+	void Start () {
+		bullets = new List<Transform>();
+	}
+	
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space)){
+
+		}
+	}
+}
